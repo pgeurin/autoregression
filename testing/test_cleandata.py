6ebdbdb7ee67983647f8cd213aa47e10975886bc
Philip Geurin <philip.geurin@gmail.com>
2018-07-26 16:03:28 -0700

    add test_rename_columns

--- testing/test_cleandata.py
+++ testing/test_cleandata.py
@@ -1,21 +1,27 @@
 import unittest
 import pandas as pd
+import numpy as np
 from autoregression.cleandata import rename_columns
 
 class TestClean(unittest.TestCase):
     def test_rename_columns(self):
         df = pd.DataFrame([[1, 2, 3, 4], [1, 2, 3, 4]], columns=['First', 'Second_and THIRD', '__Fourth', 'FIF.TH aND sixth'])
         df_snake = pd.DataFrame([[1, 2, 3, 4], [1, 2, 3, 4]], columns=['first', 'second_and_third', '_fourth', 'fif_th_and_sixth'])
-        print(df)
-        print(df_snake)
         df_rename = rename_columns(df)
-        print(df_rename)
         self.assertTrue(all(x == y
                             for x, y in zip(df_snake.columns, df_rename.columns)))
-        # self.assertEqual(all(x == y
-        #                      for x, y in zip(df_snake.values, df_rename.values)))
-        # self.assertTrue(all(x == y
-        #                     for x, y in zip(df_snake.index, df_rename.index)))
+        # NOTE: Indexes aren't the same bc one is indexed strings '0', '1' and the other is ints 0, 1
+        # print(df)
+        # print(df_snake)
+        # print(df_rename)
+        # print(df_snake.index)
+        # print(df_rename.index)
+        # print(list(df_snake.index))
+        # print(list(df_rename.index))
+        #
+        # self.assertTrue(np.testing.assert_array_equal(df_snake.values, df_rename.values))
+        self.assertTrue((df_snake.values == df_rename.values).all())
+        # self.assertTrue(list(df_snake.index) == list(df_rename.index))
         # self.assertTrue(df_snake.equals(df_rename))
 
 if __name__ == "__main__":
